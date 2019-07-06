ce434ee4ead95ee77e2bf8fbf1ba230ac2fefd1b
Dipanjan Bhowmik <onlydevelop@gmail.com>
2019-07-06 14:32:16 +0530

    Added api gw version

---
+++ lambda/activity_tracker_api.js
@@ -0,0 +1,33 @@
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'ap-south-1'});
+const docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10', region: 'ap-south-1'});
+
+exports.handler = (event, context, callback) => {
+  console.log(event.body);
+  
+  var params = {
+    TableName: 'activity',
+    Item: JSON.parse(event.body)
+  };
+
+
+  var status = "";
+  docClient.put(params, function(err, data) {
+    if (err) {
+      console.log(err);
+      status = "Error";
+    } else {
+      console.log(data);
+      status = "Success";
+    }
+  });
+
+  var response = {
+        "statusCode": 200,
+        "body": status
+    };
+  callback(null, response);
+  
+};
+
+
