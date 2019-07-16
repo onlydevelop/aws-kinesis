5e96370d43e0cc577317f0be841f265c06e9d2c2
Dipanjan Bhowmik <onlydevelop@gmail.com>
2019-07-17 00:20:47 +0530

    Added basic activity report

---
+++ lambda/get_activity_report_api.js
@@ -0,0 +1,45 @@
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'ap-south-1'});
+const docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10', region: 'ap-south-1'});
+
+exports.handler = (event, context, callback) => {
+
+    if (event.queryStringParameters != undefined) {
+        var subject = event.queryStringParameters.subject;
+        var topic = event.queryStringParameters.topic;
+    }
+
+    let queryParameters = {
+        TableName: 'activity',
+        KeyConditionExpression: "#SubTopicKey = :sub and #Timestamp > :Timestamp",
+        FilterExpression: '#Correct = :Correct',
+        ExpressionAttributeNames:{
+            "#SubTopicKey": "sub_topic",
+            "#Timestamp": "start_time",
+            "#Correct": "correct"
+        },
+        ExpressionAttributeValues: {
+            ":sub": subject + '-' + topic,
+            ":Timestamp": Math.floor((new Date().getTime() - (7*24*60*60*1000))/1000),
+            ":Correct": "yes"
+        }
+    };
+
+    docClient.query(queryParameters, function(err,data){
+        if(err){
+            var error = {
+                'statusCode': 500,
+                'body': err
+            }
+            callback(err, null);
+        }else{
+            var response = {
+                'statusCode': 200,
+                'headers': { 'Content-Type': 'application/json' },
+                'body': JSON.stringify(data)
+            }
+            console.log(JSON.stringify(data))
+            callback(null,response);
+        }
+    });
+};
